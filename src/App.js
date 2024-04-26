1013a8389ce7ea8bda0450c235f4264c739212fc
Belinda Caylor <belinda.caylor@gmail.com>
2024-04-26 12:37:32 -0400

    Add app functionality

--- src/App.js
+++ src/App.js
@@ -8,8 +8,19 @@ import Location from './components/Location';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <AppProvider>
+        <div className='container'>
+            <h1 className='mt-3'>Shopping App</h1>
+            <div className='d-flex flex-row flex-grow-1 gap-4'>
+                <CartValue />
+                <Location />
+            </div>
+            <h2>Shopping Cart</h2>
+            <ExpenseList />
+            <h2>Add Items Cart</h2>
+            <ItemSelected />
+        </div>
+    </AppProvider>
   );
 }
 
--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -1,1 +1,107 @@
-import React, { createContext, useReducer } from 'react';
+import React, { createContext, useReducer, useEffect } from 'react';
+
+export const AppReducer = (state, action) => {
+    let new_expenses = [];
+    switch (action.type) {
+        case 'ADD_QUANTITY':
+            // let updatedqty = false;
+            state.expenses.map((expense)=>{
+                if(expense.name === action.payload.name) {
+                    expense.quantity = expense.quantity + action.payload.quantity;
+                    // updatedqty = true;
+                }
+                new_expenses.push(expense);
+                return true;
+            })
+            state.expenses = new_expenses;
+            action.type = "DONE";
+            return {
+                ...state,
+            };
+
+        case 'RED_QUANTITY':
+            state.expenses.map((expense)=>{
+                if(expense.name === action.payload.name) {
+                    expense.quantity = expense.quantity - action.payload.quantity;
+                }
+                expense.quantity = expense.quantity < 0 ? 0: expense.quantity;
+                new_expenses.push(expense);
+                return true;
+            })
+            state.expenses = new_expenses;
+            action.type = "DONE";
+            return {
+                ...state,
+            };
+
+        case 'DELETE_ITEM':
+            state.expenses.map((expense)=>{
+                if(expense.name === action.payload.name) {
+                    expense.quantity = 0;
+                }
+                new_expenses.push(expense);
+                return true;
+            })
+            state.expenses = new_expenses;
+            action.type = "DONE";
+            return {
+                ...state,
+            };
+        
+        case 'CHG_LOCATION':
+            action.type = "DONE";
+            state.Location = action.payload;
+            return {
+                ...state
+            }
+
+        case 'SET_CART_VALUE':
+            return { ...state, CartValue: action.payload };
+
+            default:
+                return state;
+        }
+};
+
+const initialState = {
+    expenses: [
+        { id: "Shirt", name: 'Shirt', quantity: 0, unitprice: 500 },
+        { id: "Jeans", name: 'Jeans', quantity: 0, unitprice: 300 },
+        { id: "Dress", name: 'Dress', quantity: 0, unitprice: 400 },
+        { id: "Dinner set", name: 'Dinner set', quantity: 0, unitprice: 600 },
+        { id: "Bags", name: 'Bags', quantity: 0, unitprice: 200 },
+    ],
+    Location: '£'
+};
+
+export const AppContext = createContext();
+
+export const AppProvider = (props) => {
+    const [state, dispatch] = useReducer(AppReducer, initialState);
+
+    const calculateTotalExpense = () => {
+        const total = state.expenses.reduce((total, item) => total + (item.unitprice * item.quantity), 0);
+        dispatch({ type: 'SET_CART_VALUE', payload: total });
+      };
+    
+      useEffect(() => {
+        calculateTotalExpense(); // Calculate totalExpense initially
+      }, []);
+    
+      useEffect(() => {
+        calculateTotalExpense(); // Recalculate totalExpense when expenses change
+      }, [state.expenses]);
+
+    return (
+        <AppContext.Provider
+            value={{
+                expenses: state.expenses,
+                CartValue: state.CartValue,
+                dispatch,
+                Location: state.Location
+            }}
+        >
+            {props.children}
+        </AppContext.Provider>
+    );
+};
